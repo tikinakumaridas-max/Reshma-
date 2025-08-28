94bcf9c46ba4458a75b8c0afa4782dcdf614ba79
deploybot12 <deploybot467@gmail.com>
2025-08-28 16:29:09 +0500

    Update restart.js

--- Priyansh/commands/restart.js
+++ Priyansh/commands/restart.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "restart",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "HAMZI X FATI",
 	description: "Restart Bot",
 	commandCategory: "system",
 	usages: "",
@@ -11,5 +11,8 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	const { threadID, messageID } = event;
-	return api.sendMessage(`${global.config.BOTNAME} Bot are now Restarting Thori der wait kar munaaw kia jaldi haiw janu bula rhi haiw kia...`, threadID, () => process.exit(1));
-}
+	return api.sendMessage(`𝐁𝐎𝐓 𝐈𝐒 𝐑𝐄𝐒𝐓𝐒𝐓  🤖 👈 ●============================================================●𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐇𝐀𝐌𝐙𝐈 𝐗 𝐅𝐀𝐓𝐈 𝐁𝐨𝐭 😇👈
+=𝐎𝐰𝐧𝐞𝐫 ➻    🌹 𝐇𝐚𝐦𝐳𝐢 𝐓𝐫𝐢𝐜𝐤𝐞𝐫 🌹
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝 𝐥𝐢𝐧𝐤 😊 👈👈👈👈👈 ____________𝐇𝐀𝐌𝐙𝐈 𝐓𝐑𝐈𝐂𝐊𝐄𝐑 :- ☞ https://www.facebook.com/profile.php?id=61565881853433&mibextid=ZbWKwL
+●============================================================● 𝐁𝐎𝐓 𝐑𝐄𝐒𝐓𝐒𝐓 𝐇𝐎 𝐑𝐄𝐇𝐀 𝐇𝐀𝐈 𝐏𝐋𝐙 𝐖𝐀𝐈𝐓`, threadID, () => process.exit(1));
+		}
